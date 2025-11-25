cc750b02d70adf55e91c6eae4292b7b0fc4fa887
MacWachira <macrusdavid@gmail.com>
2025-11-25 15:52:44 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'church-site-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/media/logo.jpg',
+  '/media/preview.jpg',
+  '/media/hero1.jpg',
+  '/media/hero3.jpg',
+  '/media/hero4.jpg',
+  '/media/hero5.jpg',
+  '/media/church_1.jpg',
+  '/media/church.jpg'
+  // Add more media files as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
